3043654d4da205e7b4d96d7060306223ac162b02
wallisson de jesus campos <wallisson_wjc@outlook.com.br>
2022-12-21 14:29:42 -0300

    Finalizado 1.0

---
+++ Assets/GameFolder/Ui/Tutorial/Texto.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texto : MonoBehaviour
+{
+    public GameObject Textoo;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Textoo.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D Other)
+    {
+        if (Other.CompareTag("Player"))
+        {
+            Textoo.SetActive(false);
+        }
+    }
+}
